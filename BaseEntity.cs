8aa2ef522bd52ee0fd9355261557fef30dceef67
方宇超 <fangyuchao@veichi.com>
2025-08-08 08:44:20 +0800

    Create BaseEntity.cs
    2

---
+++ BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Veichi.SRM.Model.Base
+{
+    public class BaseEntityGuid
+    {
+        [SugarColumn(IsPrimaryKey = true)]
+        public string id { get; set; } = Guid.NewGuid().ToString();
+    }
+}
